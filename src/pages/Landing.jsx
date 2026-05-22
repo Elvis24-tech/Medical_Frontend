cd1909a1fb0ce01fca3cffc4ed3d08607c7ef415
elvis <kariuki.elvis@student.moringaschool.com>
2026-05-22 19:19:37 +0300

    Wired APIs

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,41 @@
+function Landing(){
+
+    return(
+    
+    <div
+    className="min-h-screen
+    bg-blue-600
+    text-white
+    flex
+    items-center
+    justify-center"
+    >
+    
+    <div>
+    
+    <h1
+    className="
+    text-6xl
+    font-black
+    "
+    >
+    
+    Medicore
+    
+    </h1>
+    
+    <p>
+    
+    Hospital Management System
+    
+    </p>
+    
+    </div>
+    
+    </div>
+    
+    )
+    
+    }
+    
+    export default Landing
